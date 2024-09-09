94ed2ce4f899030ccd23eccdc59ed32d81455a34
Novosad Ivan <novosad-94@list.ru>
2024-09-09 11:29:50 +0300

    CodeRun: 548_fix-all-misprints

--- ML/Easy/548_fix-all-misprints/solution.py
+++ ML/Easy/548_fix-all-misprints/solution.py
@@ -1,70 +1,33 @@
-import numpy as np
-import pandas as pd
+from utils import *
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
-from utils import *
 from tqdm import tqdm
 from joblib import Parallel, delayed
-from bruteForceFind import process_query_brute, get_all_misprints, process_query_lite
-from ErrorFixes import get_all_misprints_for_fixes, read
 
 
-def process_query(query, vectorizer, nbrs, words_list):
-    if query in words_list:
+def process_query(query, set_of_all_words, vectorizer, nbrs, list_of_words, alphabet):
+    if query in set_of_all_words:
         return f'{query} 0'
     _, indices = nbrs.kneighbors(vectorizer.transform([query]))
-    closest_words = [words_list[index] for index in indices[0]]
-    lite_result = process_query_lite(query, closest_words)
-    if lite_result != '404': 
-        return lite_result
+    closest_words = [list_of_words[index] for index in indices[0]]
+    lite_result = find_lite(query, closest_words, alphabet)
+    if '3+' in lite_result or '404' in lite_result:
+        return find_brute(query, set_of_all_words, alphabet)
     else:
-        full =  process_query_brute(query, words_list)
-        if full[-5:] == 'Error': 
-            return get_all_misprints_for_fixes(*full.split(' '))
-        else:
-            return full
+        return lite_result
 
 
 def main():
-    words_set = read_words()
-    words_list = list(words_set)
-    queries_list = read_queries()
-    print("Data loading complete. Training...")
-
-    vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(2, 3), strip_accents='unicode')
-    X = vectorizer.fit_transform(words_list)
+    set_of_all_words, list_of_all_queries, alphabet = get_data()
+    list_of_words = list(set_of_all_words)
+    vectorizer = TfidfVectorizer(
+        analyzer='char', ngram_range=(2, 3), strip_accents='unicode')
+    X = vectorizer.fit_transform(list_of_words)
     nbrs = NearestNeighbors(n_neighbors=50, algorithm='auto', metric='cosine').fit(X)
+    result = Parallel(n_jobs=-1)(delayed(process_query)(query, set_of_all_words, vectorizer, nbrs, list_of_words, alphabet) for query in tqdm(list_of_all_queries, desc='Searching for closest words'))
+        
+    save_to_file(result)
 
-    # Parallel processing of queries
-    num_cores = -1  # Use all available cores
-    results = Parallel(n_jobs=num_cores)(
-        delayed(process_query)(query, vectorizer, nbrs, words_list) for query in tqdm(queries_list, desc='Searching for closest words')
-    )
-    # results = []
-    # for query in tqdm(queries_list, desc='Searching for closest words'):
-    #     results.append(process_query(query, vectorizer, nbrs, words_list))
-
-    save_result(results, 'result.txt')
-
-def fix_mistake_Nans():
-    words_set = read_words()
-    words_list = list(words_set)
-    queries_list = read()
-    print("Data loading complete. Training...")
-
-    vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(2, 3), strip_accents='unicode')
-    X = vectorizer.fit_transform(words_list)
-    nbrs = NearestNeighbors(n_neighbors=50, algorithm='auto', metric='cosine').fit(X)
-
-    result_fixed = []
-
-    for query in tqdm(queries_list, desc='Searching for closest words'):
-        if not query or query == "None" or query[0] == "N":
-            result_fixed.append(process_query_brute(query, words_list))
-        else:
-            result_fixed.append(query)
-    save_result(result_fixed, 'fixed_result.txt')
 
 if __name__ == '__main__':
     main()
-    fix_mistake_Nans()
--- ML/Easy/548_fix-all-misprints/utils.py
+++ ML/Easy/548_fix-all-misprints/utils.py
@@ -1,127 +1,138 @@
-import re
-import sys
-
-
-def single_word(word, set_of_words):
-    alphabet = sorted(set('абвгдеёжзийклмнопрстуфхцчшщъыьэюяc'))
-
-    def generate_words_with_typos(word_to_create):
-        typos = set()
-        for char in alphabet:
-            typos.add(word_to_create + char)
-            typos.add(char + word_to_create)
-        # Single character typos
-        for i in range(len(word_to_create)):
-            # Deletion
-            typos.add(word_to_create[:i] + word_to_create[i+1:])
-            for char in alphabet:
-                # Substitution
-                typos.add(word_to_create[:i] + char + word_to_create[i+1:])
-                # Insertion
-                typos.add(word_to_create[:i] + char + word_to_create[i:])
-            if i < len(word_to_create) - 1:
-                # Swap
-                typos.add(word_to_create[:i] + word_to_create[i+1] +
-                          word_to_create[i] + word_to_create[i+2:])
-
-        return typos
-    
-    if word in set_of_words:
-        return word
-    word_typos = generate_words_with_typos(word)
-    for typo in word_typos:
-        if typo in set_of_words:
-            return typo
-    return "404"
-
-
-def check_words_with_typos(word, set_of_words):
-    alphabet = sorted(set('абвгдеёжзийклмнопрстуфхцчшщъыьэюяc'))
-
-    def generate_words_with_typos(word_to_create):
-        typos = set()
-        for char in alphabet:
-            typos.add(word_to_create + char)
-            typos.add(char + word_to_create)
-        # Single character typos
-        for i in range(len(word_to_create)):
-            # Deletion
-            typos.add(word_to_create[:i] + word_to_create[i+1:])
-            for char in alphabet:
-                # Substitution
-                typos.add(word_to_create[:i] + char + word_to_create[i+1:])
-                # Insertion
-                typos.add(word_to_create[:i] + char + word_to_create[i:])
-            if i < len(word_to_create) - 1:
-                # Swap
-                typos.add(word_to_create[:i] + word_to_create[i+1] +
-                          word_to_create[i] + word_to_create[i+2:])
-
-        return typos
-
-    if word in set_of_words:
-        return f'{word} 0'
-
-    # Check with one typo
-    typos = generate_words_with_typos(word)
-    intersection_level_1 = set_of_words.intersection(typos)
-    if intersection_level_1:
-        return f'{word} 1 {intersection_level_1.pop()}'
-
-    # check with two typos
-    words_typos = set()
-    for word_from_dict in set_of_words:
-        words_typos.update(generate_words_with_typos(word_from_dict))
-    mid = words_typos.intersection(typos)
-
-    if mid:
-        new_word = mid.pop()
-        return f'{word} 2 {new_word} ' + single_word(new_word, set_of_words)
-
-    return word + "404"
-
-
-def read_words(file_path='dict.txt'):
+def create_all(list_of_words, aplphabet):
     result = set()
-    for line in open(file_path, 'r').readlines():
-        result.add(line.strip())
+    for word in list_of_words:
+        result.update(all_typos(word, aplphabet))
     return result
 
 
-def read_queries(file_path='queries.txt'):
-    result = []
-    for line in open(file_path, 'r').readlines():
-        result.append(line.strip())
+def save_to_file(result):
+    with open('result.txt', 'w') as f:
+        for word in result:
+            f.write(f'{word}\n')
+    print(f'Result saved to result.txt')
+
+def all_typos(word, alphabet):
+    # return the set of all possible typos for the given word:
+    # 1. deletion (deleting one character from the word including the start and end)
+    # 2. substitution (changing one character in the word)
+    # 3. insertion (adding one character to the word, even at the start and end)
+    # 4. transposition (swapping two adjacent characters)
+    result = set()
+    # deletion
+    for i in range(len(word)):
+        result.add(word[:i] + word[i+1:])
+        # substitution & insertion
+        for char in alphabet:
+            result.add(word[:i] + char + word[i+1:])
+            result.add(word[:i] + char + word[i:])
+    for char in alphabet:
+        result.add(word + char)
+    # transposition
+    for i in range(len(word) - 1):
+        result.add(word[:i] + word[i+1] + word[i] + word[i+2:])
     return result
+
+
+def find_lite(word, set_closest_words, aplphabet):
+    if word in set_closest_words:
+        return f'{word} 0'
+    l1_query = create_all([word], aplphabet)
+    for l1_word in l1_query:
+        if l1_word in set_closest_words:
+            return f'{word} 1 {l1_word}'
+    l1_words = create_all(set_closest_words, aplphabet)
+    # check if there is a word in the l1_words which is also in l1_query and return if not
+    common = l1_words.intersection(l1_query)
+    if len(common) == 0:
+        return f'{word} 3+'
+    else:
+        mid_word = common.pop()
+        return f'{word} 2 {mid_word} {find_mid(mid_word, set_closest_words, aplphabet)}'
+
+
+def find_mid(mid_word, l0_words, alphabet):
+    l1_mid_word = create_all([mid_word], alphabet)
+    for typo in l1_mid_word:
+        if typo in l0_words:
+            return typo
+    return '404'
+
+
+def find_brute(query, set_of_words, aplphabet):
+    if query in set_of_words:
+        return f'{query} 0'
+    l1_query = create_all([query], aplphabet)
+    for l1_word in l1_query:
+        if l1_word in set_of_words:
+            return f'{query} 1 {l1_word}'
+    for l1_word in l1_query:
+        l2_words = create_all([l1_word], aplphabet)
+        for l2_word in l2_words:
+            if l2_word in set_of_words:
+                return f'{query} 2 {l1_word} {l2_word}'
+    return f'{query} 3+'
+
+
+def test_typos():
+    word = 'hello'
+    alphabet = set('abc')
+    deletions = {'ello', 'hllo', 'helo', 'hell'}
+    insertions = {
+        'ahello', 'bhello', 'chello',  # insertions at start
+        'haello', 'hbello', 'hcello',  # insertions after first character
+        'heallo', 'hebllo', 'hecllo',  # insertions after second character
+        'helalo', 'helblo', 'helclo',  # insertions after third character
+        'hellao', 'hellbo', 'hellco',  # insertions after fourth character
+        'helloa', 'hellob', 'helloc'  # insertions at end
+    }
+    substitutions = {
+        'aello', 'bello', 'cello',  # substitutions for first character
+        'hallo', 'hbllo', 'hcllo',  # substitutions for second character
+        'healo', 'heblo', 'heclo',  # substitutions for third character
+        'helao', 'helbo', 'helco',  # substitutions for fourth character
+        'hella', 'hellb', 'hellc'  # substitutions for fifth character
+    }
+    transpositions = {
+        'ehllo', 'hlelo', 'hello', 'helol'}
+    expected_typos = deletions.union(insertions, substitutions, transpositions)
+    typos = create_all([word], alphabet)
+    assert typos == expected_typos, f'Expected {expected_typos}, got {typos}'
+    print('Typos generated correctly')
+
+
+def test_words():
+    set_closest_words = {'яхта', 'метро', 'такси'}
+    queries = ['яхты', 'емтто', 'такси', 'автобус']
+    alph = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+    assert find_lite('яхты', set_closest_words, alph) == 'яхты 1 яхта'
+    assert find_lite('емтто', set_closest_words, alph) == 'емтто 2 емтро метро' or find_lite(
+        'емтто', set_closest_words, alph) == 'емтто 2 метто метро'
+    assert find_lite('такси', set_closest_words, alph) == 'такси 0'
+    assert find_lite('автобус', set_closest_words, alph) == 'автобус 3+'
+
+    assert find_brute('яхты', set_closest_words, alph) == 'яхты 1 яхта'
+    assert find_brute('емтто', set_closest_words, alph) == 'емтто 2 емтро метро' or find_brute(
+        'емтто', set_closest_words, alph) == 'емтто 2 метто метро'
+    assert find_brute('такси', set_closest_words, alph) == 'такси 0'
+    assert find_brute('автобус', set_closest_words, alph) == 'автобус 3+'
+
+    print("Test set passed")
 
 
 def get_data():
-    words = read_words()
-    queries = read_queries()
-    return words, queries
-
-
-def save_result(result, file_path='result.txt'):
-    with open(file_path, 'w') as f:
-        for word in result:
-            f.write(f'{word}\n')
-    print(f'Result saved to {file_path}')
-
-
-def test():
-    dict = {'яхта', 'метро', 'такси'}
-    queries = ['яхты', 'емтто', 'такси', 'автобус']
-    for query in queries:
-        print(check_words_with_typos(query, dict))
-
-
-def read_test_result(file_path='result_test.txt'):
-    data = []
-    with open(file_path, 'r') as f:
-        for line in f.readlines():
-            data.append(line.strip())
-    return data
+    set_of_all_words = set()
+    list_of_all_queries = []
+    with open('dict.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            set_of_all_words.add(line.strip())
+    with open('queries.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            list_of_all_queries.append(line.strip())
+    alphabet = {'л', 'ж', 'в', 'ь', 'ю', 'э', 'о', 'т', 'м', 'а', 'я', 'щ', 'с', 'д', 'п', 'з', 'ц',
+                'ш', 'е', 'c', 'ъ', 'ф', 'б', 'г', 'й', 'ч', 'ы', ' ', 'р', 'у', 'и', 'н', 'х', 'к'}
+    return set_of_all_words, list_of_all_queries, alphabet
 
 
 if __name__ == '__main__':
-    test()
+    test_typos()
+    test_words()
